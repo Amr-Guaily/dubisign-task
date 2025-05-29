5f16358a7d34b77d8a6b432cb9aed816afa90972
Amr-Guaily <amrguaily@gmail.com>
2025-05-29 15:08:03 +0300

    ✨ add product, cart item, and price range interfaces

---
+++ app/types/index.ts
@@ -0,0 +1,16 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+}
+
+export interface CartItem extends Product {
+  quantity: number;
+}
+
+export interface PriceRange {
+  min: number;
+  max: number;
+}
